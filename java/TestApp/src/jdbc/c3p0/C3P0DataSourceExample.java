708c5a20226bcb004758b150a490490d194d3877
Hardik <hardik@hardik.com>
2017-11-01 18:52:49 +0530

    Added java jdbc examples

---
+++ java/TestApp/src/jdbc/c3p0/C3P0DataSourceExample.java
@@ -0,0 +1,74 @@
+package jdbc.c3p0;
+
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author hardiku
+ */
+public class C3P0DataSourceExample
+{
+
+    public static void main(String[] args) throws PropertyVetoException, SQLException, IOException
+    {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try
+        {
+            connection = DataSource.getInstance().getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from employee");
+            while(resultSet.next())
+            {
+                System.out.println("employeeid: " + resultSet.getString("employeeid"));
+                System.out.println("employeename: " + resultSet.getString("employeename"));
+            }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(resultSet != null)
+            {
+                try
+                {
+                    resultSet.close();
+                }
+                catch(SQLException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if(statement != null)
+            {
+                try
+                {
+                    statement.close();
+                }
+                catch(SQLException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if(connection != null)
+            {
+                try
+                {
+                    connection.close();
+                }
+                catch(SQLException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
